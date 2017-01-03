c95d78961b2a6b53f2ebbaafe06242d4e5e68e63
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-01-02 21:05:18 -0800

    routes working from what I can tell

--- lab-carolina/route/movie-route.js
+++ lab-carolina/route/movie-route.js
@@ -2,8 +2,10 @@
 
 const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
+const createError = require('http-errors');
 
 const Song = require('../model/song.js');
+const Movie = require('../model/movie.js');
 
 const movieRouter = module.exports = new Router();
 
@@ -12,4 +14,17 @@ movieRouter.post('/api/song/:songID/movie', jsonParser, function(req, res, next)
   Song.findByIdAndAddMovie(req.params.songID, req.body)
   .then( movie => res.json(movie))
   .catch(next);
+});
+
+movieRouter.get('/api/movie/:id', function(req, res, next){
+  Movie.findById(req.params.id)
+  .populate('_song')
+  .then(movie => res.json(movie))
+  .catch(err => next(createError(404, err.message)));
+});
+
+movieRouter.delete('/api/movie/:movieID', function(req, res, next){
+  Movie.remove(req.params.id)
+  .then(movie => res.json(movie))
+  .catch(err => next(createError(404, err.message)));
 });
